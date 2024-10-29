5d27c0541f80b68dfe9fb7404ec28a35ba1ad3f5
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2024-10-29 18:01:43 +0300

    about & contact edits

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -8,7 +8,7 @@
 
 <section id="about-us-page" class="py-5">
     <div class="container text-center">
-        <div class="card border-0 p-4 rounded-4">
+        <div class="border-0 p-4 rounded-4">
             <a href="https://suhail.ae"><img src="{{ asset('front/imgs/sohail-logo.png') }}" height="150" class="mb-4"></a>
             <h1 class="fw-bold fs-1 mb-4">@lang('custom.about.title')</h1>
             <p class="fs-5">@lang('custom.about.p1')</p>
@@ -18,7 +18,7 @@
             <p class="fs-5">@lang('custom.about.p5')</p>
             <p class="fs-5">@lang('custom.about.p6')</p>
             <p class="fs-5 mb-0">@lang('custom.about.p7')</p>
-            <a href="https://suhail.ae" class="fw-bold">suhail.ae</a>
+            <a href="https://suhail.ae" class="fw-bold btn btn-primary mt-3 px-3"><i class="fa-regular fa-share-from-square me-2"></i> suhail.ae</a>
         </div>
     </div>
 </section>
