7c6aad5ee25ba683a4e1e256fa93966ff9359932
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2024-11-12 12:21:03 +0200

    Update single-article.blade.php

--- resources/views/front/news/single-article.blade.php
+++ resources/views/front/news/single-article.blade.php
@@ -18,7 +18,7 @@
             <div class="col-12">
                 <div class="col-12 d-flex flex-wrap gap-4 mt-3">
                     <div class="col-lg-9 col-12 card border-0 rounded-3 p-3">
-                        <div class="article-image-holder d-flex justify-content-center align-items-center w-100 mb-2">
+                        <div class="article-image-holder d-flex justify-content-center align-items-center w-100 mb-3">
                             <img src="{{ asset('storage/' . $article->cover) }}" class="rounded-3" alt="{{ $article->title }}" title="{{ $article->title }}">
                         </div>
                         <h1 class="text-center fs-2">{{ $article->title }}</h1>
