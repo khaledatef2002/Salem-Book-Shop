d7606f41a3118a7b0b3d01f2a59e120d5b4fd3ea
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2024-11-13 13:20:12 +0200

    Update _js-libs.blade.php

--- resources/views/front/partials/_js-libs.blade.php
+++ resources/views/front/partials/_js-libs.blade.php
@@ -3,7 +3,7 @@
 </script>
 <script src="{{ asset('front') }}/libs/bootstrap/js/bootstrap.min.js" async></script>
 <script src="{{ asset('front') }}/libs/jquery/jquery-3.6.4.min.js" defer></script>
-<script src="{{ asset('front') }}/libs/owlcarousel/owl.carousel.min.js" async></script>
+<script src="{{ asset('front') }}/libs/owlcarousel/owl.carousel.min.js" defer></script>
 <script src="{{ asset('front') }}/libs/sweetalert2/sweet.js" async></script>
 <script src="{{ asset('front') }}/js/minimized/main.min.js" defer></script>
 @yield('custom-js')
