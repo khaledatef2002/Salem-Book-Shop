23da69d88f9468189f3903040f34815ddc858a24
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2024-11-13 13:01:58 +0200

    minimize js

--- resources/views/front/partials/_js-libs.blade.php
+++ resources/views/front/partials/_js-libs.blade.php
@@ -3,7 +3,7 @@
 </script>
 <script src="{{ asset('front') }}/libs/bootstrap/js/bootstrap.min.js"></script>
 <script src="{{ asset('front') }}/libs/jquery/jquery-3.6.4.min.js"></script>
-<script src="{{ asset('front') }}/libs/owlcarousel/owl.carousel.js"></script>
+<script src="{{ asset('front') }}/libs/owlcarousel/owl.carousel.min.js"></script>
 <script src="{{ asset('front') }}/libs/sweetalert2/sweet.js"></script>
-<script src="{{ asset('front') }}/js/main.js"></script>
+<script src="{{ asset('front') }}/js/minimized/main.min.js"></script>
 @yield('custom-js')
