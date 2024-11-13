a5296e1a23535f3eb933138bf2258570ff681b56
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2024-11-13 13:07:12 +0200

    Update _js-libs.blade.php

--- resources/views/front/partials/_js-libs.blade.php
+++ resources/views/front/partials/_js-libs.blade.php
@@ -1,9 +1,9 @@
 <script>
     var rtl = {{ LaravelLocalization::getCurrentLocaleDirection() == "rtl" ? "true" : "false" }}
 </script>
-<script src="{{ asset('front') }}/libs/bootstrap/js/bootstrap.min.js"></script>
-<script src="{{ asset('front') }}/libs/jquery/jquery-3.6.4.min.js"></script>
-<script src="{{ asset('front') }}/libs/owlcarousel/owl.carousel.min.js"></script>
-<script src="{{ asset('front') }}/libs/sweetalert2/sweet.js"></script>
-<script src="{{ asset('front') }}/js/minimized/main.min.js"></script>
+<script src="{{ asset('front') }}/libs/bootstrap/js/bootstrap.min.js" async></script>
+<script src="{{ asset('front') }}/libs/jquery/jquery-3.6.4.min.js" defer></script>
+<script src="{{ asset('front') }}/libs/owlcarousel/owl.carousel.min.js" async></script>
+<script src="{{ asset('front') }}/libs/sweetalert2/sweet.js" async></script>
+<script src="{{ asset('front') }}/js/minimized/main.min.js" defer></script>
 @yield('custom-js')
