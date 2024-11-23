ad97d60d6c8c24759c8786f079462458b2a802c7
Khaled Atef <39334335+khaledatef2002@users.noreply.github.com>
2024-11-23 13:42:08 +0200

    Create 2024_11_18_214038_create_book_requests_table.php

---
+++ database/migrations/2024_11_18_214038_create_book_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\BookRequestsStatesType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book_requests', function (Blueprint $table) {
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')->on('books')->references('id')->onDelete('cascade')->onUpdate('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->on('users')->references('id')->onDelete('cascade')->onUpdate('cascade');
+            $table->primary(['book_id', 'user_id']);
+            $table->enum('state', [BookRequestsStatesType::approved->value, BookRequestsStatesType::pending->value, BookRequestsStatesType::canceled->value]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book_requests');
+    }
+};
